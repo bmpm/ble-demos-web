5044c6f1f4cd21221a916dd99c95774f2f7c2f2e
Bruna Moreira <bruna.moreira@gmail.com>
2014-02-06 13:36:56 -0400

    Add utilities functions
    Add function to clean a list and for adding a new header in a list.

--- js/ble.js
+++ js/ble.js
@@ -1,5 +1,34 @@
 var bus = null;
 
+function addHeaderList(header, spin, list, ulId) {
+  var devListContainer = document.getElementById(list);
+  var item = document.createElement("header");
+
+  item.appendChild(document.createTextNode(header));
+  if (spin != null) {
+    var spinItem = document.createElement("progress");
+    spinItem.id = spin;
+    spinItem.style.marginLeft = "15px";
+    spinItem.style.marginBottom = "10px";
+    item.appendChild(spinItem);
+  }
+  devListContainer.appendChild(item);
+
+  var devList = document.createElement("ul");
+  devList.id = ulId;
+  devListContainer.appendChild(devList);
+
+  return devList;
+}
+
+function clearAllList(list) {
+  listView = document.getElementById(list);
+
+  while (listView.hasChildNodes()) {
+      listView.removeChild(listView.lastChild);
+  }
+}
+
 function errorCB(error) {
   console.log("error: " + error + "\n");
 }
